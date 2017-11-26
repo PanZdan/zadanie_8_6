f973ab5bcb0f7a00e8e21aa816031ff00db24f35
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-11-26 20:00:23 +0100

    Add index html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+//var name = prompt('Enter your name');
+
+// var a = 12,
+// 	h = 12,
+// 	triangleArea = a*h/2;
+// console.log('Triangle field with base a: '+ a + ' and height h: ' + h + ' is equal to: ' +triangleArea);
+
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
+
+var a = prompt ('Enter base a'),
+	h = prompt ('Enter height h'),
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: '+ a + ' and height h: ' + h + ' is equal to: ' +triangleArea);
