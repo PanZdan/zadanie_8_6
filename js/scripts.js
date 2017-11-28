48428ac5cb2b2b9ee4bda730bb46dc7859ee8095
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-11-28 22:36:15 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,14 @@
 var a = prompt ('Enter (a)'),
 	b = prompt ('Enter (b)'),
 	value = (a * a) - (2 * a * b) + (b * b);
-  console.log('The result of the equation, where "a" is equal: '+ a + ' and "b": ' + b + ' is equal to: ' +value);
+console.log('The result of the equation, where "a" is equal: ' + a + ' and "b": ' + b + ' is equal to: ' + value);
 
 if (value < 0 ) {
-  console.log('Wynik jest ujemny');
-} else if 
-  (value > 0) {
-  console.log('Wynik jest dodatni');
+	console.log('Wynik jest ujemny');
+} else if (value > 0) {
+	console.log('Wynik jest dodatni');
 } else {
-  console.log('Wynik jest równy 0');
+	console.log('Wynik jest równy 0');
 }
 // if (value < 0 ) {
 //   console.log('Wynik jest ujemny');
